d79e76067829f909b30a957de657f69b92de0461
leedanbi <neco950@naver.com>
2021-11-23 17:31:17 +0900

    feat : Add 3 list
    To test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,15 @@
+fruits = ['Apple', 'Banana', 'Pineapple']
+
+animals = [
+	'Lion',
+	'Tiger',
+	'Rabbit'
+]
+Clubs = [
+	'MansU',
+	'ManC',
+]
+
 for i in range(1,16+1):
 	if i%15==0:
 		print('fizzbuzz')
